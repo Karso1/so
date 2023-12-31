1bf085bf891d9748915ff0e1c4073c65b24889d5
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-12-31 17:01:43 +0800

    Add files via upload

---
+++ hello.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier:MIT
+//版权声明
+
+//声明合约文件的版本
+pragma solidity ^0.8.7;
+//^表示8.7以上的都可以编译，8.0以内的，如8.1，8.2，8.9.超过9则不行
+
+contract HelloWorld//定义一个合约
+{
+    string public myString = "hello world";//public表示公开变量
+
+}
