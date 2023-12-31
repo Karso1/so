0bd72c014bfdc535d5296611d00d4cf18669e027
Karso1 <129362623+Karso1@users.noreply.github.com>
2023-12-31 18:43:06 +0800

    Add files via upload

---
+++ IfElse.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.7;
+
+contract IfElse
+{
+    function example(uint _X) external pure returns (uint)
+    {
+        if(_x<10)
+        {
+            return 1;
+        }
+        else if(_x < 20)
+        {
+            return 2;
+        }
+        else {
+            return 3;
+        }
+    }
+
+    function ternary(uint _x) external pure return s(uint)
+    {
+        return _x<10 ? 1:2;
+    }
+}
